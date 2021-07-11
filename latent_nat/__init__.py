ab59543d962504c6730a9f514342c26cd008f118
Bao-Yu <baoy@smail.nju.edu.cn>
2021-07-11 09:44:40 +0800

    Update __init__.py

--- latent_nat/__init__.py
+++ latent_nat/__init__.py
@@ -1,7 +1,7 @@
 from latent_nat import awesome_nat
 from latent_nat import awesome_nat_loss
 from latent_nat import cnat
-from latent_nat import flat
+from latent_nat import glat
 from latent_nat import nat_task
 from latent_nat import vnat
 
@@ -12,5 +12,4 @@ __all__ = [
     "awesome_nat",
     "vnat",
     "cnat",
-    "latent_glat"
 ]
